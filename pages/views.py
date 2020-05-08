c6913fefd08300288a0f79402356f09588a4ad4f
alpden550 <alpden550@gmail.com>
2020-05-08 14:21:19 +0300

    Updated main page, added three last listings.

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,20 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView
+
+from listings.models import Listing
 
 
-class MainPageView(TemplateView):
+class MainPageView(ListView):
+    """View for main page."""
+
+    model = Listing
+    queryset = Listing.objects.select_related(
+        'realtor',
+    ).order_by(
+        '-created_at',
+    ).filter(
+        is_published=True,
+    )[:3]
+    context_object_name = 'listings'
     template_name = 'pages/index.html'
 
 

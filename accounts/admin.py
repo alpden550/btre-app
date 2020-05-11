e1789a06760fd2852c88667a96a0be770776ef15
alpden550 <alpden550@gmail.com>
2020-05-11 18:51:35 +0300

    Added contact into admin.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from accounts.models import Contact
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'listing', 'name', 'phone', 'email', 'created_at')
+    list_display_links = ('id', 'name')
+    search_fields = ('listing', 'name')
+    list_filter = ('listing',)

2e26ab63081b909bee9d1df9fa47cffec05c36f8
alpden550 <alpden550@gmail.com>
2020-05-12 21:37:54 +0300

    Added installing docker-compose for fabric.

--- fabfile.py
+++ fabfile.py
@@ -13,5 +13,12 @@ def install_packages():
     sudo('apt-get install -y {}'.format(' '.join(packages)))
 
 
+def install_docker_compose():
+    sudo('curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose')
+    sudo('chmod +x /usr/local/bin/docker-compose')
+    sudo('ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose')
+
+
 def deploy():
     install_packages()
+    install_docker_compose()

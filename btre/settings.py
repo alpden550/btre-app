322138033001341bfbf029511ffe7dd8549dd65b
alpden550 <alpden550@gmail.com>
2020-05-13 08:35:08 +0300

    Changed redis port.

--- btre/settings.py
+++ btre/settings.py
@@ -174,7 +174,7 @@ EMAIL_USE_LOCALTIME = True
 
 # Redis config
 REDIS_HOST = 'localhost'
-REDIS_PORT = '6379'
+REDIS_PORT = '6380'
 BROKER_URL = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
 BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
 CELERY_RESULT_BACKEND = 'redis://' + REDIS_HOST + ':' + REDIS_PORT + '/0'
